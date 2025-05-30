da3302ea3b3f758da8a2ba779899c7786323af1a
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-05-30 09:21:51 +0200

    Introduce webgraphs

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,33 +1,47 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
-
 #[macro_use]
 extern crate rocket;
 
-use rocket::serde::{Serialize, json::Json};
+use rocket::serde::{json::Json, Serialize};
+use webgraph::{
+    prelude::BvGraph,
+    traits::{RandomAccessGraph, RandomAccessLabeling, SequentialLabeling},
+};
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
 struct Neighborhood {
-    vertex: usize,
+    graph_name: String,
+    num_nodes: usize,
+    num_arcs: u64,
+    vertex_id: usize,
+    outdegree: usize,
     neighborhood: Vec<usize>,
 }
 
-#[get("/neighborhood/<graph>/<vertex>")]
-fn neighborhood(graph: &str, vertex: usize) -> Json<Neighborhood> {
+#[get("/neighborhood/<topic>/<graph_name>/<vertex_id>")]
+fn neighborhood(topic: &str, graph_name: &str, vertex_id: usize) -> Json<Neighborhood> {
+    let path = format!("/usr/webgraphs/{}/{}", topic, graph_name);
+    let graph = BvGraph::with_basename(&path).load().unwrap();
+
     Json(Neighborhood {
-        vertex,
-        neighborhood: vec![1, 2, 3], // Example data
+        graph_name: graph_name.to_string(),
+        num_nodes: graph.num_nodes(),
+        num_arcs: graph.num_arcs(),
+        vertex_id,
+        neighborhood: graph.successors(vertex_id).collect(),
+        outdegree: graph.outdegree(vertex_id),
     })
 }
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: &str, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+#[get("/")]
+fn echo() -> &'static str {
+    "Usage: 
+    
+        GET /webgraph-api/neighborhood/<topic>/<graph_name>/<vertex_id>
+            Returns the neighborhood of a vertex in the specified graph under the given topic."
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/webgraph-api", routes![neighborhood,hello])
+    rocket::build().mount("/webgraph-api", routes![neighborhood, echo])
 }
