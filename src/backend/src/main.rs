438484097a7d00e9290a20397a3c1a9d0a2a0718
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-06-21 14:46:57 +0200

    Added harmonic histogram

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,9 +1,12 @@
 #[macro_use]
 extern crate rocket;
 
-use std::sync::Arc;
+use std::{
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
-use avgdist_rs::avgdist_sample;
+use avgdist_rs::{avgdist_sample, hc_sample};
 use rocket::serde::{Deserialize, Serialize, json::Json};
 use webgraph::{
     prelude::BvGraph,
@@ -23,6 +26,14 @@ struct AvgdistRequest {
 struct AvgdistResponse {
     diameter: usize,
     distance: f64,
+    elapsed: Duration,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct HarmonicCentralityResponse {
+    centralities: Vec<(usize, f64)>,
+    elapsed: Duration,
 }
 
 #[derive(Serialize)]
@@ -68,6 +79,7 @@ fn avgdist(topic: &str, graph_name: &str, params: Json<AvgdistRequest>) -> Json<
         .build()
         .expect("Failed to create thread pool");
 
+    let instant = Instant::now();
     let tuple = avgdist_sample(
         &thread_pool,
         params.sample_size,
@@ -78,6 +90,58 @@ fn avgdist(topic: &str, graph_name: &str, params: Json<AvgdistRequest>) -> Json<
     Json(AvgdistResponse {
         diameter: tuple.0,
         distance: tuple.3 / (tuple.4 as f64),
+        elapsed: instant.elapsed(),
+    })
+}
+
+#[post("/harmonic_centrality/<topic>/<graph_name>", data = "<params>")]
+fn harmonic_centrality(
+    topic: &str,
+    graph_name: &str,
+    params: Json<AvgdistRequest>,
+) -> Json<HarmonicCentralityResponse> {
+    let path = format!("/usr/webgraphs/{}/{}-t", topic, graph_name);
+    let graph = BvGraph::with_basename(&path).load().unwrap();
+    let num_nodes = graph.num_nodes();
+    let thread_pool = rayon::ThreadPoolBuilder::default()
+        .num_threads(params.num_threads)
+        .build()
+        .expect("Failed to create thread pool");
+
+    let arc_graph = Arc::new(graph);
+    let instant = Instant::now();
+    let (_dia, _sum, _count, v_sizes, v_finite_ds) =
+        hc_sample(&thread_pool, params.sample_size, arc_graph, params.exact);
+
+    let mut sizes = vec![0usize; num_nodes];
+    let mut sums = vec![None; num_nodes];
+
+    for i in 0..num_nodes {
+        sizes[i] += v_sizes[i];
+
+        if let Some(sum) = v_finite_ds[i] {
+            let existing_sum = sums[i].unwrap_or(0.0);
+            sums[i] = Some(existing_sum + sum);
+        }
+    }
+
+    let normalization = (params.sample_size as f64).recip();
+
+    let mut centralities: Vec<(usize, f64)> = (0..num_nodes)
+        .filter_map(|node| {
+            if let Some(sum) = sums[node] {
+                Some((node, normalization * sum))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    centralities.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+
+    Json(HarmonicCentralityResponse {
+        centralities: centralities,
+        elapsed: instant.elapsed(),
     })
 }
 
@@ -147,6 +211,13 @@ fn echo() -> &'static str {
 fn rocket() -> _ {
     rocket::build().mount(
         "/webgraph-api",
-        routes![avgdist, neighborhood_get, neighborhood, summary, echo],
+        routes![
+            harmonic_centrality,
+            avgdist,
+            neighborhood_get,
+            neighborhood,
+            summary,
+            echo
+        ],
     )
 }
